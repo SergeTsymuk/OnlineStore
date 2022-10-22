1d20fc4956d156a8285a3382f602f78beb6fc2c5
Serge <Serge.tsymuk@gmail.com>
2022-10-22 16:05:13 -0400

    OnlineStore
    OnlineStore: FirstCommit

---
+++ OnlineStore/OnlineStore/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Models;
+
+namespace OnlineStore.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<Page> Pages { get; set; }
+        public StoreContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-890Q20G; Initial Catalog=OnlineStore; Integrated Security = True;");
+        }
+    }
+}
