3c97e33969c2475dbac781cf75588778280ea04f
Serge <Serge.tsymuk@gmail.com>
2022-10-22 17:21:23 -0400

    Admin Area Added
    Admin Area Added to the project

---
+++ OnlineStore/OnlineStore/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Data;
+using OnlineStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PagesController : Controller
+    {
+        private readonly StoreContext context;
+        public PagesController(StoreContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+            List<Page> pagesList = await pages.ToListAsync();
+            return View(pagesList);
+        }
+    }
+}
--- OnlineStore/OnlineStore/Startup.cs
+++ OnlineStore/OnlineStore/Startup.cs
@@ -47,6 +47,11 @@ namespace OnlineStore
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
