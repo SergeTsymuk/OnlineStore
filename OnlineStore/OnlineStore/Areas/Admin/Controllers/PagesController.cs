dbd045325406172b7bc7dbd0ea69679b02bbeb8f
Serge <Serge.tsymuk@gmail.com>
2022-10-24 13:43:43 -0400

    Added Create Page
    Added Create Page to the Pages View

--- OnlineStore/OnlineStore/Areas/Admin/Controllers/PagesController.cs
+++ OnlineStore/OnlineStore/Areas/Admin/Controllers/PagesController.cs
@@ -16,11 +16,28 @@ namespace OnlineStore.Areas.Admin.Controllers
         {
             this.context = context;
         }
+
+        // GET RREQUEST FOR / Admin / Pages 
         public async Task<IActionResult> Index()
         {
             IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
             List<Page> pagesList = await pages.ToListAsync();
             return View(pagesList);
         }
+
+        // GET RREQUEST FOR / Admin / Pages / Details / ID: 5
+        public async Task<IActionResult> Details(int id)
+        {
+            Page page = await context.Pages.FirstOrDefaultAsync(x => x.Id == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+            return View(page);
+        }
+
+        // ADMIN / PAGES / CREATE 
+        public IActionResult Create() => View();
+
     }
 }
